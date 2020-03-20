10c3476edbdc005d4f7e1a51f637538c02526a2e
Yang Liu <thomasliuy@gmail.com>
2020-03-19 22:49:10 -0400

    add doc strings

--- chem_reader/mol2_reader.py
+++ chem_reader/mol2_reader.py
@@ -29,6 +29,9 @@ class Mol2Reader:
             return self._n_mols
     
     def get_blocks(self):
+        r""" Read the blocks in .mol2 file based on @<TRIPOS>MOLECULE label.
+        return (list): list of block contents as strings.
+        """
         block_starts = [i for i, l in enumerate(self.file_contents)
                         if "@<TRIPOS>MOLECULE" in l]
         blocks = list()
@@ -94,6 +97,11 @@ class Mol2Filter:
     __filters = {"ghose": ghose_filter}
     
     def write_out(self, content, outfile, compress=True):
+        r""" Write content to outfile. Compress with gzip if compress is True.
+        content (str): content for writing into outfile
+        outfile (str): output file path. The path must exist.
+        compress (bool): if to compress the output file with gzip.
+        """
         if compress:
             with open(outfile, "wb") as outf:
                 outf.write(gzip.compress(content.encode("utf-8")))
@@ -107,7 +115,17 @@ class Mol2Filter:
                    method="ghose",
                    exclude_salt=False,
                    compress=True):
-        r"""
+        r""" Filter all the .mol2 or .gz files with ghose filter in the
+        directory. The molecules will be writen into the file with the same
+        filename as the input files. Empty files will NOT be written out.
+        dirpath (str): diretory contains the .mol2 or .gz files. Directory with
+                       subfolders is acceptable.
+        outpath (str): the output diretory path. The directory will be created
+                       if it does not exist.
+        method (str): method to filter the molecules. Default is Ghose filter.
+        exclude_salt (bool): if to filter out the molecules containing salt
+                             atoms.
+        compress (bool): if True, the output file will be compressed with gzip.
         """
         outpath = os.path.abspath(outpath)
         os.makedirs(outpath, exist_ok=True)

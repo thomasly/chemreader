39c45ccace9a3080d1c8f9e8becc57d1303e0f50
Yang Liu <thomasliuy@gmail.com>
2020-03-28 22:04:38 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="chemreader",
+    version="0.0.1",
+    author="Yang Liu",
+    author_email="thomasliuy@gmail.com",
+    description="Read data from typical chemical file formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires='>=3.6',
+)

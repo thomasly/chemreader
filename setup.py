2b4d873e0180ef7ad63f23c2cac779807ebc8455
Yang Liu <thomasliuy@gmail.com>
2020-07-16 21:19:43 -0400

    update setup

--- setup.py
+++ setup.py
@@ -12,12 +12,14 @@ setup(
     description="Read data from typical chemical file formats",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/thomasly/chem_reader",
+    download_url="https://github.com/thomasly/chem_reader/archive/0.2.1.tar.gz",
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy", "scipy", "rdkit"],
+    install_requires=["numpy", "scipy"],
     python_requires=">=3.6",
 )
